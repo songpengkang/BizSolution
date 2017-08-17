449007ada9714caa6e4f99ce2cfbdbf9425321ae
songpengkang <908711505@qq.com>
2017-08-17 18:03:34 +0800

    Default

--- javascript/main.js
+++ javascript/main.js
@@ -10,16 +10,61 @@
         $picLiWidth = $picLi.width(),
         $tab = $('.banner .tab'),
         $tabLi = $tab.find("li"),
-        $btnLeft = $(".banner .left"),
-        $btnRight = $(".banner .right");
+        $btn = $(".banner .btn div"),
+        index = 0;     //tabLi的下表
+
+    //初始化
+    $tabLi.eq(0).addClass("on");
 
     // 点击tab li 的时候
     $tabLi.click(function () {
+        var x = $(this).index();
+        change(x);
+    });
+    //点击左右按钮的时候
+    $btn.click(function () {
+        var x = index;
+        console.log($(this).index());
+        if($(this).index()){
+            x++;
+            x%=$tabLi.length;
+        }else {
+            x--;
+            if(x<0) x = $tabLi.length - 1;
+        }
+        change(x);
+    });
+    //自动轮播
+    auto();
+    function auto() {
+        setInterval(function () {
+            var x = index;
+            x++;
+            x%=$tabLi.length;
+            change(x);
+        },3000);
+        return auto;
+    }
+    function change(i) {
         $pic.stop().animate({
-            "marginLeft" : -$picLiWidth*($(this).index()+1)+"px"
+            "marginLeft" : -$picLiWidth*(i+1)+"px"
         },800);
-        $(this).addClass("on").siblings().removeClass("on");
+        $tabLi.eq(i).addClass("on").siblings().removeClass("on");
+        index = i;
+    }
+})();
+
+
+/*********导航栏吸顶************************/
+(function () {
+    $(window).scroll(function () {
+        var $mainPage = $("#mainPage"),
+            $navBar = $mainPage.find(".navbar"),
+            $mainPageScrollHeight = $(document).offsetTop,
+            $navHeight = $("#nav").height;
+            if($mainPageScrollHeight > $navHeight){
+                $navBar.css("backgroundColor","rgba(0,0,0,.3)")
+            }
+        console.log($mainPageScrollHeight);
     })
-
-
 })();
