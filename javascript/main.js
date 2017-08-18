4700a0624c54988876e76426c6bba784f31da076
songpengkang <908711505@qq.com>
2017-08-18 18:15:09 +0800

    Default

--- javascript/main.js
+++ javascript/main.js
@@ -11,7 +11,9 @@
         $tab = $('.banner .tab'),
         $tabLi = $tab.find("li"),
         $btn = $(".banner .btn div"),
-        index = 0;     //tabLi的下表
+        index = 0,     //tabLi的下表
+        clickTime = 0,
+        timer1 = null;
 
     //初始化
     $tabLi.eq(0).addClass("on");
@@ -22,22 +24,26 @@
         change(x);
     });
     //点击左右按钮的时候
+    $btn.hover(function () {
+        clearInterval(timer1)
+    },auto());
     $btn.click(function () {
-        var x = index;
-        console.log($(this).index());
-        if($(this).index()){
-            x++;
-            x%=$tabLi.length;
-        }else {
-            x--;
-            if(x<0) x = $tabLi.length - 1;
-        }
-        change(x);
+       if(new Date - clickTime > 800){
+           var x = index;
+           if($(this).index()){
+               x++;
+               x%=$tabLi.length;
+           }else {
+               x--;
+               if(x<0) x = $tabLi.length - 1;
+           }
+           change(x);
+           clickTime = new Date;
+       }
     });
     //自动轮播
-    auto();
     function auto() {
-        setInterval(function () {
+        timer1 = setInterval(function () {
             var x = index;
             x++;
             x%=$tabLi.length;
@@ -58,13 +64,39 @@
 /*********导航栏吸顶************************/
 (function () {
     $(window).scroll(function () {
-        var $mainPage = $("#mainPage"),
-            $navBar = $mainPage.find(".navbar"),
-            $mainPageScrollHeight = $(document).offsetTop,
-            $navHeight = $("#nav").height;
-            if($mainPageScrollHeight > $navHeight){
-                $navBar.css("backgroundColor","rgba(0,0,0,.3)")
-            }
-        console.log($mainPageScrollHeight);
+        var docScrollTop = $(document).scrollTop(),
+            navHeight = $("#nav").height(),
+            $navbar = $(".navbar"),
+            $navbarUla= $(".navbar .daohang li a");
+        if(docScrollTop < navHeight ){
+            $navbar.css({
+                backgroundColor:"rgba(0,0,0,.5)",
+            })
+            $navbarUla.css({
+                color : "#d5d5d5"
+            })
+        }else {
+            $navbar.css({
+                backgroundColor:"rgba(255,255,255,.8)",
+            })
+            $navbarUla.css({
+                color : "#333"
+            })
+        }
+
+    })
+})();
+
+/********************背景移动**********************************/
+(function () {
+    $(window).scroll(function () {
+        var $fix = $(".fix"),
+            fixHeight = $fix.height(),
+            docHeight = $(document).height(),
+            docHeightScrollTop = $(document).scrollTop(),
+            positionY = fixHeight / docHeight * docHeightScrollTop ;
+        console.log(positionY);
+        $fix.css("backgroundPosition","center -" + positionY + "px");
+
     })
 })();
